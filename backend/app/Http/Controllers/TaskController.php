50c3a0718c5ce0798917e16b23d7aaff6aaff5d6
Davi Cezar <davicezarpe@gmail.com>
2025-07-22 23:53:33 -0300

    Initial commit: To-Do List Laravel API

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+        'title' => 'required|string|max:255',
+        ]);
+        $task = Task::create($validatedData);
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $validatedData = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'completed' => 'sometimes|boolean',
+        ]);
+
+        // 2. Atualiza a tarefa com os dados validados
+        $task->update($validatedData);
+
+        // 3. Retorna a tarefa atualizada como JSON
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
